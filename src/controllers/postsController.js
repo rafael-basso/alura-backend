ddfe8d73ea1a95e8c7803edf4446a3612337eef5
rafael-basso <rafael_basso81@hotmail.com>
2024-11-20 13:00:21 -0300

    inserir validação pesquisa por id

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -9,7 +9,21 @@ export async function listarPosts(req, res) {
 export async function listarPostPorId(req, res) {
     const id = String(req.params.id);
     
-    const post = await getPostPorId(id);
-
-    return res.status(200).json(post);
+    if (id.length < 24) {
+        return res.status(400).json({
+            status: res.statusCode,
+            message: "Incorrect ID value."
+        });
+    } else {
+        const post = await getPostPorId(id);
+        
+        if (post) {
+            return res.status(200).json(post);
+        } else {
+            return res.status(404).json({
+                status: res.statusCode,
+                message: "Post not found."
+            });
+        }
+    }
 }
